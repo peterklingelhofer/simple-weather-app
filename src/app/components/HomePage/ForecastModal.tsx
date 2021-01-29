2c865de9f3b52b0df0128352292ca72a24166ee2
Peter Klingelhofer <peterklingelhofer@gmail.com>
2021-01-28 18:11:03 -0600

    (add) Implemented Material UI table for weather forecast data on location click

--- src/app/components/HomePage/ForecastModal.tsx
+++ src/app/components/HomePage/ForecastModal.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Modal from 'react-modal';
 import { Button } from '@material-ui/core';
-import { kelvinToFahrenheit } from '../../../utils/conversionHelpers';
+import ForecastTable from './ForecastTable';
 
 interface ForecastModalProps {
   toggleModal: any;
@@ -13,76 +12,8 @@ interface ForecastModalProps {
   >;
 }
 
-interface Forecast {
-  dt: number;
-  temp: number;
-  humidity: number;
-  pressure: number;
-  clouds: number;
-  wind_speed: number;
-  wind_deg: number;
-  weather: { main: string; description: string }[];
-}
-
 const ForecastModal: React.FC<ForecastModalProps> = props => {
-  const forecast = useSelector((state: any) => state.weather);
   const { toggleModal, isOpen, weatherHeader } = props;
-  if (!forecast) {
-    return <></>;
-  }
-
-  const forecastTableBody = forecast[0].hourly.map((row: Forecast) => {
-    const {
-      dt,
-      temp,
-      humidity,
-      pressure,
-      clouds,
-      wind_speed: windSpeed,
-      wind_deg: windDegree,
-      weather,
-    } = row;
-    return (
-      <tbody>
-        <tr key={dt.toString()}>
-          <td>{new Date(dt * 1000).toString()}</td>
-          <td>{kelvinToFahrenheit(temp)}°F</td>
-          <td>{humidity}%</td>
-          <td>{pressure} atm</td>
-          <td>{clouds} %</td>
-          <td>{windSpeed} m/s</td>
-          <td>{windDegree}°</td>
-          <td>{weather[0].main}</td>
-          <td>{weather[0].description}</td>
-        </tr>
-      </tbody>
-    );
-  });
-
-  const forecastTableHead = (
-    <thead>
-      <tr>
-        <th>Datetime</th>
-        <th>Temperature</th>
-        <th>Humidity</th>
-        <th>Pressure</th>
-        <th>Clouds</th>
-        <th>Wind speed</th>
-        <th>Wind degree</th>
-        <th>Weather</th>
-        <th>Description</th>
-      </tr>
-    </thead>
-  );
-
-  const forecastTable = (
-    <div>
-      <table className="table">
-        {forecastTableHead}
-        {forecastTableBody}
-      </table>
-    </div>
-  );
 
   return (
     <Modal
@@ -95,7 +26,7 @@ const ForecastModal: React.FC<ForecastModalProps> = props => {
         Close
       </Button>
       <h1>{weatherHeader}</h1>
-      {forecastTable}
+      <ForecastTable key={Math.random()} />
     </Modal>
   );
 };
---
+++ src/app/components/HomePage/ForecastTable.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { kelvinToFahrenheit } from '../../../utils/conversionHelpers';
+
+interface Forecast {
+  dt: number;
+  temp: number;
+  humidity: number;
+  pressure: number;
+  clouds: number;
+  wind_speed: number;
+  wind_deg: number;
+  weather: { main: string; description: string }[];
+}
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+export default function ForecastTable() {
+  const classes = useStyles();
+
+  const forecast = useSelector((state: any) => state.weather);
+
+  const forecastTableBody = forecast[0].hourly.map((row: Forecast) => {
+    const {
+      dt,
+      temp,
+      humidity,
+      pressure,
+      clouds,
+      wind_speed: windSpeed,
+      wind_deg: windDegree,
+      weather,
+    } = row;
+    return (
+      <TableBody>
+        <TableRow key={dt.toString()}>
+          <TableCell component="th" scope="row">
+            {new Date(dt * 1000).toString()}
+          </TableCell>
+          <TableCell align="right">{kelvinToFahrenheit(temp)}°F</TableCell>
+          <TableCell align="right">{humidity}%</TableCell>
+          <TableCell align="right">{pressure} atm</TableCell>
+          <TableCell align="right">{clouds} %</TableCell>
+          <TableCell align="right">{windSpeed} m/s</TableCell>
+          <TableCell align="right">{windDegree}°</TableCell>
+          <TableCell align="right">{weather[0].main}</TableCell>
+          <TableCell align="right">{weather[0].description}</TableCell>
+        </TableRow>
+      </TableBody>
+    );
+  });
+
+  const forecastTableHead = (
+    <TableHead>
+      <TableRow>
+        <TableCell>Datetime</TableCell>
+        <TableCell align="right">Temperature</TableCell>
+        <TableCell align="right">Humidity</TableCell>
+        <TableCell align="right">Pressure</TableCell>
+        <TableCell align="right">Clouds</TableCell>
+        <TableCell align="right">Wind speed</TableCell>
+        <TableCell align="right">Wind degree</TableCell>
+        <TableCell align="right">Weather</TableCell>
+        <TableCell align="right">Description</TableCell>
+      </TableRow>
+    </TableHead>
+  );
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        {forecastTableHead}
+        {forecastTableBody}
+      </Table>
+    </TableContainer>
+  );
+}
